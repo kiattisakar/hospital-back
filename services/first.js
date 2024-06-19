f216e3c789676e9074bf345520ff7d2ca671af69
LAPTOP-8PA22L2C\fAm NoteBook <kiattisak.ar@rmuti.ac.th>
2024-06-19 14:31:25 +0700

    แก้ api เพิ่มการกรองข้อมูล

--- services/first.js
+++ services/first.js
@@ -11,34 +11,24 @@ async function first(req, res) {
 
         // คำสั่ง SQL สำหรับดึงข้อมูลที่ต้องการจากตาราง
         let query = `
-            SELECT
-                dbo.ms_ward.wardcode,
-                ('[' + dbo.ms_ward.wardcode + '] ' + dbo.ms_ward.warddesc) AS warddesc,
-                COUNT(DISTINCT dbo.ms_patientadmit.an) AS countadmit
-            FROM
-                dbo.ms_patient
-                LEFT JOIN dbo.ms_patientadmit ON dbo.ms_patient.hn = dbo.ms_patientadmit.hn
-                LEFT JOIN dbo.ms_bedmove ON dbo.ms_patientadmit.an = dbo.ms_bedmove.an
-                LEFT JOIN dbo.ms_ward ON dbo.ms_bedmove.towardcode = dbo.ms_ward.wardcode
-                LEFT JOIN dbo.ms_patientDischarge ON dbo.ms_patientadmit.an = dbo.ms_patientDischarge.an 
-                    AND dbo.ms_patientadmit.hn = dbo.ms_patientDischarge.hn
-            WHERE
-                dbo.ms_bedmove.status = 0
-                AND dbo.ms_ward.wardcode NOT IN ('1420')
-                ${ptstatus}
-        `;
-
-        if (search !== '') {
-            query += ` AND dbo.ms_patientadmit.an = '${search}' `;
-        }
-
-        query += `
-            GROUP BY
-                dbo.ms_ward.wardcode,
-                ('[' + dbo.ms_ward.wardcode + '] ' + dbo.ms_ward.warddesc)
-            ORDER BY
-                CONVERT(INT, dbo.ms_ward.wardcode) ASC
-        `;
+        SELECT    
+                dbo.ms_ward.wardcode,    
+                ('[' + dbo.ms_ward.wardcode + '] ' + dbo.ms_ward.warddesc) as warddesc,     
+                count(DISTINCT dbo.ms_patientadmit.an) As countadmit     
+           FROM    
+                dbo.ms_patient    
+                Left Join dbo.ms_patientadmit ON dbo.ms_patient.hn = dbo.ms_patientadmit.hn     
+                Left Join dbo.ms_bedmove ON dbo.ms_patientadmit.an = dbo.ms_bedmove.an     
+                Left Join dbo.ms_ward ON dbo.ms_bedmove.towardcode = dbo.ms_ward.wardcode     
+                Left Join dbo.ms_patientDischarge ON  dbo.ms_patientadmit.an = dbo.ms_patientDischarge.an And dbo.ms_patientadmit.hn = dbo.ms_patientDischarge.hn     
+           WHERE   dbo.ms_bedmove.status = 0   
+                And dbo.ms_ward.wardcode Not IN ('1420')   
+                AND dbo.ms_patientadmit.DCdatetime is null 
+           GROUP BY   
+                dbo.ms_ward.wardcode,    
+                ('[' + dbo.ms_ward.wardcode + '] ' + dbo.ms_ward.warddesc)    
+                ORDER BY CONVERT(INT,ms_ward.wardcode) ASC
+           `;
 
         // ส่งคำสั่ง SQL ไปยังฐานข้อมูล
         const result = await pool.request()
